93caa980b932b7f338ceac95ceb800fb81c6fb97
Tolga BAYRAK <bayraktolga28@gmail.com>
2022-12-06 13:03:30 +0300

    Jwt ile ilgili fonksiyonların yazılma işlemi

--- server/routes/auth.py
+++ server/routes/auth.py
@@ -1,8 +1,16 @@
 from flask import request,jsonify, Blueprint
+from service.auth_service import login_service, signup_service
+
 auth_route = Blueprint('user_route', __name__)
 
 
-@auth_route.route("/api/v1/auth/register", methods=['POST'])
+@auth_route.route("/api/v1/auth/login", methods=['POST'])
 def login_user():
     data = request.get_json()
-    return jsonify(data)
+    return login_service(data)
+
+
+@auth_route.route("/api/v1/auth/register", methods=['POST'])
+def signup_user():
+    data = request.get_json()
+    return signup_service(data) 
--- server/service/auth_service.py
+++ server/service/auth_service.py
@@ -1,5 +1,8 @@
 from model.user import User
 from flask import make_response
+from utils.password_encryption import encrypt_password, compare_passwords
+from utils.jwt_token import generate_token
+import datetime
 
 def signup_service(userdata):
     try:
@@ -13,7 +16,7 @@ def signup_service(userdata):
             new_user = User(username=username, password=password)
 
             new_user.save()
-            return make_response({'message': 'User succesfully created'}, 200)
+            return make_response({'message': 'User succesfully created'}, 201)
 
     except Exception as e:
         return make_response({'message': str(e)}, 404)
@@ -26,4 +29,12 @@ def login_service(user_credentials):
             return {"status": 404, "message": "email does not exists"}
         else:
             for user in email_check:
-
+                payload = {"email": user['email'], "_id": str(user['id']), 'exp': datetime.datetime.utcnow(
+                ) + datetime.timedelta(minutes=60)}
+                if compare_passwords(user_credentials['password'], user['password']):
+                    token = generate_token(payload, "SecretKey")
+                    return make_response({'token': token}, 200)
+                else:
+                    return make_response({'message': 'Invalid password'}, 403)
+    except Exception as e:
+        return make_response({'message': str(e)}, 404)

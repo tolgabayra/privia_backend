0edc89fcaf5c470be87d4f625666e534c1b5210d
Tolga BAYRAK <bayraktolga28@gmail.com>
2022-12-06 19:51:40 +0300

    Login ve register sayfalarının axios istekleri yazıldı

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,28 @@
 import React from 'react'
+import { useState } from 'react'
+import { appAxios } from "../utils/appAxios"
+
 
 export default function Login() {
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+
+
+  const submitLogin = () => {
+    appAxios.post("/auth/login",{
+      username,
+      password
+    },{withCredentials: true})
+    .then((res) => {
+      console.log(res);
+    })
+    .catch(err=>{
+
+    })
+  }
+
+
+
   return (
     <div>
       <div className="flex h-screen bg-indigo-700">
@@ -8,19 +30,19 @@ export default function Login() {
           <header>
             <img className="w-20 mx-auto mb-5" src="https://img.icons8.com/fluent/344/year-of-tiger.png" />
           </header>
-          <form>
+          <div>
             <div>
               <label className="block mb-2 text-indigo-500" for="username">Username</label>
-              <input className="w-full p-2 mb-6 text-indigo-700 border-b-2 border-indigo-500 outline-none focus:bg-gray-300" type="text" name="username" />
+              <input onChange={(e)=>setUsername(e.target.value)} className="w-full p-2 mb-6 text-indigo-700 border-b-2 border-indigo-500 outline-none focus:bg-gray-300" type="text" name="username" />
             </div>
             <div>
               <label className="block mb-2 text-indigo-500" for="password">Password</label>
-              <input className="w-full p-2 mb-6 text-indigo-700 border-b-2 border-indigo-500 outline-none focus:bg-gray-300" type="password" name="password" />
+              <input onChange={(e)=>setPassword(e.target.value)}  className="w-full p-2 mb-6 text-indigo-700 border-b-2 border-indigo-500 outline-none focus:bg-gray-300" type="password" name="password" />
             </div>
             <div>
-              <button className="w-full bg-indigo-700 hover:bg-indigo-800 text-white font-bold py-2 px-4 mb-6 rounded" type="submit" >Log In</button>
+              <button onClick={submitLogin} className="w-full bg-indigo-700 hover:bg-indigo-800 text-white font-bold py-2 px-4 mb-6 rounded" type="submit" >Log In</button>
             </div>
-          </form>
+          </div>
 
         </div>
       </div>
